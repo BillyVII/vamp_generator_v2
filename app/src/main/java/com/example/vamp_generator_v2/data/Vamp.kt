59a7cb87b12ba450f693972854392ce7133029d0
BillyVII <vily1903@gmail.com>
2023-04-20 18:19:36 +0200

    homeFragment layout

---
+++ app/src/main/java/com/example/vamp_generator_v2/data/Vamp.kt
@@ -0,0 +1,9 @@
+package com.example.vamp_generator_v2.data
+
+data class Vamp(
+    val name: String,
+    val race: String,
+    val nature: String,
+    val concept: String,
+    val sire: String
+)
