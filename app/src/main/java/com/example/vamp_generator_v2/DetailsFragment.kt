9a3dd96aa21bfc56d9be59a78730dfac73433560
BillyVII <vily1903@gmail.com>
2023-04-20 22:17:04 +0200

    detail fragment et recycler view en place

--- app/src/main/java/com/example/vamp_generator_v2/DetailsFragment.kt
+++ app/src/main/java/com/example/vamp_generator_v2/DetailsFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vamp_generator_v2.adapter.ConceptAdapter
+import com.example.vamp_generator_v2.databinding.FragmentDetailsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +25,11 @@ class DetailsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
+    // TODO debut du code ici
+    private lateinit var binding: FragmentDetailsBinding
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +43,15 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = binding.detailRecyclerV
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = ConceptAdapter()
     }
 
     companion object {
